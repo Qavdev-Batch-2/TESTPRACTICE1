6337caed3c933ce4eb6b469bb644215bd88013e8
Toyosi <toyoodeyemi@gmail.com>
2023-12-16 10:13:11 +0100

    SignUp Updated

--- cypress/e2e/ToyosiSignUp.cy.js
+++ cypress/e2e/ToyosiSignUp.cy.js
@@ -1,4 +1,4 @@
-import {signup} from "../fixtures/selectors.js";
+import {signup} from "../fixtures/ToyosiSelectors";
 
 Cypress.on('uncaught:exception', (err, runnable) => {
   // returning false here prevents Cypress from
@@ -16,7 +16,11 @@ describe("SignUp Page",() => {
     cy.get(signup.signupBtn).click()
     cy.get(signup.dontHaveAnAcc).click()
     cy.get(signup.firstNameField).type('John')
-   
+    cy.get(signup.lastNameField).type('Chimezie')
+    cy.get(signup.emailAddressField).type('Joemie2@gmail.com')
+    cy.get(signup.phoneNumberField).type('08141313552')
+    cy.get(signup.passwordField).type('case@study')
+    cy.get(signup.createAccountBtn).click()
 
 
   
